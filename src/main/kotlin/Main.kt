26ba8f4e75ff2b687ad8de96aecf89fe4082239a
Taifin <tralllgm@gmail.com>
2023-02-01 12:13:15 +0100

    Dimensions and params fixes, new arguments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,43 +5,34 @@ import com.xenomachina.argparser.default
 import java.io.File
 
 data class NetworkParams(
-    val layerToFunction: Map<Int, (Double) -> Double>,
-    val biasLimits: Pair<Double, Double>
+    val activationFunctions: List<(Double) -> Double>,
+    val layerSizes: List<Int>,
+    val biasLimits: Pair<Double, Double>,
+    val numberOfLayers: Int = layerSizes.size
 )
-
-object NetworkParamsFactory {
-    fun create(processors: List<(Double) -> Double>, lo: Double, hi: Double): NetworkParams {
-        val layerToFunctionMap: Map<Int, (Double) -> Double> = ((0..processors.size).zip(processors).toMap())
-        return NetworkParams(layerToFunctionMap, Pair(lo, hi))
-    }
-}
 
 fun reLU(x: Double): Double = max(0.0, x)
 
 fun sigmoid(x: Double): Double = 1 / (1 + Math.E.pow(x))
 
-fun calculateSingleLayer(nodes: Matrix, weights: Matrix, bias: Matrix, function: (Double) -> Double): Matrix {
+fun calculateDenseLayer(nodes: Matrix, weights: Matrix, bias: Matrix, function: (Double) -> Double): Matrix {
     val weighted = weights * nodes
     return (weighted + bias).applyFunction(function)
 }
 
 fun inferenceProcess(inputNodes: Matrix, params: NetworkParams): Matrix {
     var currentLayer = inputNodes
-    for ((_, processorFunction) in params.layerToFunction) {
-        // TODO: may be wrong dimensions
-        val relationWeights = Matrix(currentLayer.rows, currentLayer.rows).fillWithRandomNumbers()
-        val bias = Matrix(currentLayer.rows, 1).fillWithRandomNumbers(params.biasLimits.first, params.biasLimits.second)
+    for (i in 0 until params.numberOfLayers) {
+        val relationWeights = Matrix(params.layerSizes[i], currentLayer.rows).fillWithRandomNumbers()
+        val bias =
+            Matrix(params.layerSizes[i], 1).fillWithRandomNumbers(params.biasLimits.first, params.biasLimits.second)
 
-        currentLayer = calculateSingleLayer(currentLayer, relationWeights, bias, processorFunction)
+        currentLayer = calculateDenseLayer(currentLayer, relationWeights, bias, params.activationFunctions[i])
     }
     return currentLayer
 }
-/*
-2048x2048 * 2048x1 = 2048x1 + 2048x1 = 2048x1
-2048x2048 * 2048x1
- */
 
-// Input matrix interpreted as a single vector of values
+// Input matrix is interpreted as a single vector
 fun readInput(inFile: File): Matrix {
     val list = mutableListOf<Double>()
     for (line in inFile.readLines()) {
@@ -53,13 +44,30 @@ fun readInput(inFile: File): Matrix {
 class Args(parser: ArgParser) {
     val source by parser.positional("source filename") { File(this) }
     val dest by parser.storing("-o", "--out", help = "destination filename") { File(this) }.default<File?>(null)
+    val generate by parser.storing(
+        "-g",
+        "--gen",
+        help = "ignore input file and generate random input vector of given size"
+    ) { toInt() }
+        .default<Int?>(null)
+    val sizes by parser.adding(
+        "-s",
+        "--size",
+        help = "specify sizes of dense layers (by default 512 and 512)"
+    ) { toInt() }.default(
+        mutableListOf(512, 512)
+    )
 }
 
 fun main(args: Array<String>) {
-    val params: NetworkParams = NetworkParamsFactory.create(listOf(::reLU, ::sigmoid), 0.0, 10.0)
 
     ArgParser(args).parseInto(::Args).run {
-        val input = readInput(source)
+        val params = NetworkParams(listOf(::reLU, ::sigmoid), sizes, Pair(0.0, 10.0))
+        val input: Matrix = if (generate == null) {
+            readInput(source)
+        } else {
+            Matrix(generate!!, 1).fillWithRandomNumbers()
+        }
         val processed = inferenceProcess(input, params)
         if (dest == null) {
             println(processed)
